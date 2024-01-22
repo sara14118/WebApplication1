166810110bdbcfed9ff1972414f110e298c74efe
sara <sara.alzadjali14@outlook.com>
2024-01-22 12:59:20 +0400

    Add project files.

---
+++ WebApplication1/Controllers/ToDoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ToDoController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<ToDo> GetAll()
+		{
+			var todos = new List<ToDo>() {
+			new ToDo { Id = 1, Name = "API", Description = "you should study api" },
+			new ToDo { Id = 2, Name = "PL", Description = "you should study PL" },
+			new ToDo { Id = 3, Name = "BLL", Description = "you should study BLL"}
+
+		};
+			return todos;
+
+		}
+	}
+	}
+
---
+++ WebApplication1/Model/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Model
+{
+	public class ToDo
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		
+	}
+}
